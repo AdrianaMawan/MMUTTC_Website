c11ed346b5d8dddc6d78917c9ec8f93ccc95d7d1
Josh <joshuatuining@gmail.com>
2024-12-25 22:34:02 +0800

    Update to code

---
+++ Website1.js
@@ -0,0 +1,14 @@
+// Smooth Scrolling for Navigation Links
+document.querySelectorAll('nav a').forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            targetSection.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        }
+    });
+});
